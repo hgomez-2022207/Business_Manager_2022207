e812a87281678e362de2760bcb09d159766f642f
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-02-28 16:32:50 -0600

    Se crea el post en usuario route

--- src/user/user.route.js
+++ src/user/user.route.js
@@ -4,5 +4,15 @@ import {check} from "express-validator"
 import { validarCampos } from "../middlewares/validar-campos.js";
 import { tieneRole } from "../middlewares/validar-roles.js";
 import { validarJWT } from "../middlewares/validar-jwt.js";
-
+import {UserPost} from "../user/user.controller.js"
 export default Router;
+
+Router.post('/',
+    [
+        check('name','invalid user').not().isEmpty(),
+        check('email','invalid email').not().isEmpty(),
+        check('password','password is not valid'),
+        validarCampos
+    ],UserPost
+
+);
