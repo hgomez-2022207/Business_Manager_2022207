7e8eaed610f337fe016c2389dc8368d6a93dc6bc
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-02-28 16:20:23 -0600

    Se define como router

---
+++ src/busines/empresa.route.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import {} from "./empresa.controller";
+import { Router } from "express";
+
+const router =  Router();
+
+export default router;
