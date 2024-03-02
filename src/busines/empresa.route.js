b4726d08b1e0f3320d241119a67ada6fd0406f84
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-02 14:41:27 -0600

    Se crea post en el route de empresa

--- src/busines/empresa.route.js
+++ src/busines/empresa.route.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
 import { check } from "express-validator";
-import {} from "./empresa.controller";
-import { Router } from "express";
-
+import {empresaPost} from "./empresa.controller.js";
+import { validarCampos } from "../middlewares/validar-campos.js";
 const router =  Router();
+
+router.post('/',
+    [
+        check('name','invalid name').not().isEmpty(),
+        check('level','unwritten level').not().isEmpty(),
+        check('age','unwritten age').not().isEmpty(),
+        check('category','unwritten category').not().isEmpty(),
+        validarCampos
+    ],empresaPost
+
+);
 
 export default router;
