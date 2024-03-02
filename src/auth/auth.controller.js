babec60df13e0575750cd7fc8913d6f01d556696
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-02 17:46:43 -0600

    Se crea el controlador de auth

---
+++ src/auth/auth.controller.js
@@ -0,0 +1,44 @@
+import bcryptjs from 'bcryptjs';
+import User from '../user/user.model.js'
+import { generarJWT } from '../helpers/generate-jwt.js'; 
+
+export const login = async (req, res) =>{
+    const { email, password } = req.body;
+
+    try{
+        const user = await User.findOne({email});
+        if (!user) {
+            return res.status(400).json({
+              msg: "Credenciales incorrectas, Correo no existe en la base de datos",
+            });
+        }
+
+        if(!user.state) {
+            return res.status(400).json({
+                msg: "El usuario no existe"
+            });
+        }
+
+        const validPassword = bcryptjs.compareSync(password, user.password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+              msg: "La contraseña es incorrecta",
+            });
+        }
+
+        const token = await generarJWT(user.id);
+
+        res.status(200).json({
+            msg: "Login correcto",
+            user,
+            token
+        });
+
+    }catch(e){
+        console.log(e);
+        res.status(500).json({
+            msg: "Comuniquese con el administrador",
+        });
+    }
+}
