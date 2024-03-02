c1822b2c96c0b4d203f9ed2fa1ec12052d7f3b72
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-02 16:19:10 -0600

    Se crea buscar por nombre en controller

--- src/busines/empresa.controller.js
+++ src/busines/empresa.controller.js
@@ -1,15 +1,24 @@
 import { response, request } from "express";
 import bcryptjs from 'bcryptjs';
-import User from './empresas.model.js';
+import Busines from './empresas.model.js';
 
 export const empresaPost = async (req, res) => {
     console.log('empresaPost');
     const { name, level, age, category } = req.body;
-    const user = new User({ name, level, age, category });
+    const empresa = new Busines({ name, level, age, category });
 
-    await user.save();
+    await empresa.save();
 
     res.status(200).json({
-        user,
+        empresa,
+    });
+}
+
+export const getEmpresaByName = async(req, res = response) =>{
+    const { name } = req.params;
+    const empresa = await Busines.findOne({name:name});
+
+    res.status(200).json({
+        empresa
     });
 }
