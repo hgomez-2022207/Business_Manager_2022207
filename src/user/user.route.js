cc9758e11aa54c1e2767f6f19e9067fc01899d28
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-02 14:08:51 -0600

    Se coorege error en crear usuario en route

--- src/user/user.route.js
+++ src/user/user.route.js
@@ -2,17 +2,22 @@ import { Router } from "express";
 import {check} from "express-validator"
 
 import { validarCampos } from "../middlewares/validar-campos.js";
-import { tieneRole } from "../middlewares/validar-roles.js";
+//import { tieneRole } from "../middlewares/validar-roles.js";
 import { validarJWT } from "../middlewares/validar-jwt.js";
-import {UserPost} from "../user/user.controller.js"
-export default Router;
+import { UserPost } from "./user.controller.js";
+import { existeEmail } from "../helpers/db-validators.js"
 
-Router.post('/',
+const router = Router();
+
+router.post('/',
     [
         check('name','invalid user').not().isEmpty(),
         check('email','invalid email').not().isEmpty().isEmail(),
+        check('email').custom(existeEmail),
         check('password','password is not valid').isLength({min:6}),
         validarCampos
     ],UserPost
 
 );
+
+export default router;
