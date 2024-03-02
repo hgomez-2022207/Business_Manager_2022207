6b0714e77ccf4a00e50500c885fb48c6b4730d74
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-02 14:40:46 -0600

    Se cra el post de crear empresa

--- src/busines/empresa.controller.js
+++ src/busines/empresa.controller.js
@@ -1,3 +1,15 @@
 import { response, request } from "express";
 import bcryptjs from 'bcryptjs';
 import User from './empresas.model.js';
+
+export const empresaPost = async (req, res) => {
+    console.log('empresaPost');
+    const { name, level, age, category } = req.body;
+    const user = new User({ name, level, age, category });
+
+    await user.save();
+
+    res.status(200).json({
+        user,
+    });
+}
